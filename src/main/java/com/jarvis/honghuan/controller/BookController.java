b9000a5782d3461ffdc2cda3be695b052cda46b7
honghuan.Liu <honghuan0921@gmail.com>
2022-08-13 12:55:40 +0800

    1

---
+++ src/main/java/com/jarvis/honghuan/controller/BookController.java
@@ -0,0 +1,11 @@
+package com.jarvis.honghuan.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author honghuan.Liu
+ * @date 2022/8/13 12:53
+ */
+@RequestMapping
+public class BookController {
+}
